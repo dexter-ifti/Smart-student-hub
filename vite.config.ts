c8e523b10512d6f87c28d994ecf5bf897a9286d8
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-09-06 23:06:52 +0530

    update Vite configuration to enable build manifest and specify output directory

--- vite.config.ts
+++ vite.config.ts
@@ -50,10 +50,7 @@
         '@': path.resolve(__dirname, './src'),
       },
     },
-    build: {
-      target: 'esnext',
-      outDir: 'dist',
-    },
+    build: { manifest: true, outDir: './dist' },
     server: {
       port: 3000,
       open: true,
